1e6207896896199b6d0d7b1c26db1f0b13f01fc6
JollanXD <128173157+JollanXD@users.noreply.github.com>
2026-04-02 18:10:12 +0300

    Update AniList Russian.user.js

--- AniList Russian.user.js
+++ AniList Russian.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AniList Russian
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @description  Перевод AniList на русский
 // @author       Jollan
 // @match        https://anilist.co/*
